bb48aa549e65ecd5f3c569c6bdf9ef05541ecf91
Rithvika Devisetti <70250513+rithvika1221@users.noreply.github.com>
2023-03-12 21:17:53 -0700

    Week 2 String First Assignments
    Week 2 String First Assignments

---
+++ src/StringsFirstAssignments/src/StringFirstAssignments/Part4.java
@@ -0,0 +1,26 @@
+package StringFirstAssignments;
+
+import edu.duke.*;
+
+
+
+public class Part4 {
+
+	public boolean findingYoutube() {
+		 URLResource ur = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+		return false;
+
+	}
+
+	public void testing(){
+	
+
+	}
+
+	public static void main(String[] args) {
+		Part4 obj1 = new Part4();
+		obj1.testing();
+		System.out.println("Class Successfull");
+
+	}
+}
